ef5d379b659bd60dc9ac17162e90775a9bb60829
239y1a05g2-svg <239y1a05g2@ksrmce.ac.in>
2026-03-01 21:18:24 +0530

    Update Local2.java

--- java-practice-from-basics-main (3)/java-practice-from-basics-main/src/Local2.java
+++ java-practice-from-basics-main (3)/java-practice-from-basics-main/src/Local2.java
@@ -37,11 +37,11 @@ public class Local2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Dupe4 d=new Dupe4("madhu",1010,87199181);
+		Dupe4 d=new Dupe4("vishnu",1010,87199181);
 	
 		//"madhu",101,618210);
 		System.out.println(d.getName()+" "+d.getId()+" "+d.getNum());
-//	Dupe1 d1=new Dupe1("madhu",100,99959959);
+//	Dupe1 d1=new Dupe1("vishnu",100,99959959);
 //		System.out.println(d1.getName()+" "+d1.getId()+" "+d1.getNum());
 
 		
